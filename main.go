b23afdee2efa3e01eec4113a094c57ec42475318
legostin <84d83ca6>
2018-05-13 19:09:12 +0600

    fixed imports

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"gocron/timechecker"
-	"gocron/types"
+	"github.com/legostin/gocron/timechecker"
+	"github.com/legostin/gocron/types"
 	"io/ioutil"
 	"log"
 	"os"
