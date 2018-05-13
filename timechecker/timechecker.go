ec1e037922e24a757f48cc493d41cfa9d25f0596
legostin <84d83ca6>
2018-05-13 18:31:25 +0600

    gofmt

--- timechecker/timechecker.go
+++ timechecker/timechecker.go
@@ -8,13 +8,13 @@ import (
 	"time"
 )
 
-// Функция для проверки необходимости запуска команды в данный момент времени
+//NeedToRunNow Функция для проверки необходимости запуска команды в данный момент времени
 func NeedToRunNow(element types.Task) bool {
 	period := element.Period
 	current := int32(time.Now().Unix())
-	current_date := time.Now().Format("2006-01-02")
-	current_time := time.Now().Format("15:04:05")
-	current_weekday := time.Now().Format("Mon")
+	currentDate := time.Now().Format("2006-01-02")
+	currentTime := time.Now().Format("15:04:05")
+	currentWeekday := time.Now().Format("Mon")
 	need := true
 	// Если задан период, то проверяем подходит ли текущее время для выполнения задачи
 	if period > 0 {
@@ -25,7 +25,7 @@ func NeedToRunNow(element types.Task) bool {
 	// Проверяем, не входит ли текущее время в период "сна" задания
 	if need {
 		for _, stime := range element.SleepTime {
-			if checkInSleepTime(stime, current_time) {
+			if checkInSleepTime(stime, currentTime) {
 				need = false
 			}
 		}
@@ -34,7 +34,7 @@ func NeedToRunNow(element types.Task) bool {
 	// Проверяем, не входит ли текущая дата в период "сна" задания
 	if need {
 		for _, sday := range element.SleepDays {
-			if checkInSleepingDays(sday, current_weekday) {
+			if checkInSleepingDays(sday, currentWeekday) {
 				need = false
 			}
 		}
@@ -44,7 +44,7 @@ func NeedToRunNow(element types.Task) bool {
 	if need && len(element.Time) > 0 {
 		temp_need := false
 		for _, stime := range element.Time {
-			if checkInTime(stime, current_time) {
+			if checkInTime(stime, currentTime) {
 				temp_need = true
 			}
 		}
@@ -57,7 +57,7 @@ func NeedToRunNow(element types.Task) bool {
 	if need && len(element.DateTime) > 0 {
 		temp_need_datetime := false
 		for _, sdatetime := range element.DateTime {
-			if checkInTime(sdatetime, current_date+" "+current_time) {
+			if checkInTime(sdatetime, currentDate+" "+currentTime) {
 				temp_need_datetime = true
 			}
 		}
@@ -70,6 +70,7 @@ func NeedToRunNow(element types.Task) bool {
 
 }
 
+//checkPeriod функция для проверки принадлежности к периоду в секундах
 func checkPeriod(period int32, current int32) bool {
 	if current%period == 0 {
 		return true
@@ -78,34 +79,39 @@ func checkPeriod(period int32, current int32) bool {
 	}
 }
 
-func checkInSleepTime(sleeprange string, current_time string) bool {
-	sleeprange_start := toInteger(strings.Split(sleeprange, "-")[0])
-	sleeprange_end := toInteger(strings.Split(sleeprange, "-")[1])
-	current_seconds := toInteger(current_time)
-	if current_seconds >= sleeprange_start && current_seconds <= sleeprange_end {
+//checkInSleepTime функция, которая определяет не входит ли текущее время в промежуток "сна" для задачи
+func checkInSleepTime(sleeprange string, currentTime string) bool {
+	sleeprangeStart := toInteger(strings.Split(sleeprange, "-")[0])
+	sleeprangeEnd := toInteger(strings.Split(sleeprange, "-")[1])
+	currentSeconds := toInteger(currentTime)
+	if currentSeconds >= sleeprangeStart && currentSeconds <= sleeprangeEnd {
 		return true
 	} else {
 		return false
 	}
 }
 
-func checkInSleepingDays(day string, current_day string) bool {
-	if day == current_day {
-		return true
-	} else {
-		return false
-	}
-}
-func checkInTime(time string, current_time string) bool {
-
-	if time == current_time {
-		fmt.Println(time, current_time)
+//checkInSleepingDays Проверка, не входил ли текущее время в дни "сна" задачи
+func checkInSleepingDays(day string, currentDay string) bool {
+	if day == currentDay {
 		return true
 	} else {
 		return false
 	}
 }
 
+//checkInTime Функция проверяет, не является ли текущее время моментом запуска задачи
+func checkInTime(time string, currentTime string) bool {
+
+	if time == currentTime {
+		fmt.Println(time, currentTime)
+		return true
+	} else {
+		return false
+	}
+}
+
+//toInteger функция для преобразования времени в секунды с полуночи
 func toInteger(time string) int {
 	hours, _ := strconv.Atoi(strings.Split(time, ":")[0])
 	minutes, _ := strconv.Atoi(strings.Split(time, ":")[1])
